25664252e0dff1bd3dfecbc3dedd58cb0a047004
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 15:42:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,9 +25,11 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while counter < array.length do
     array[counter].to_s
     counter += 1
+    new_array.push()
     return array
   end
   end
